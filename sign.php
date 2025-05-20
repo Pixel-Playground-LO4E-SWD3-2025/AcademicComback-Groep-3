33004e851b0bffa6bfc37d9dc9198c91bdacaa61
t <6045566@mborijnland.nl>
2025-05-20 10:22:52 +0200

    letter

--- sign.php
+++ sign.php
@@ -9,7 +9,7 @@
 <body>
      <footer class="footer">
   <p>&copy; 2025 00games. Alle rechten voorbehouden.</p>
-  <p>Gemaakt  door 00games Tea</p>
+  <p>Gemaakt  door 00games Team</p>
 </footer>
 </body>
 </html>
