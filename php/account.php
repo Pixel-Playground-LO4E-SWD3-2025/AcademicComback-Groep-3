946be0ec79c5a252a968c32de9320e78525f7072
t <6045566@mborijnland.nl>
2025-06-03 10:34:34 +0200

    Update account.php

--- php/account.php
+++ php/account.php
@@ -1,6 +1,23 @@
   <?php require_once '../partials/header.php'; 
   
   ?>
+
+  <?php if (isset($_POST['submit'])) {
+    if (!empty($_POST['gebruikersnaam']) && !empty($_POST['wachtwoord'])) {
+
+        try {
+            $conn = new mysqli("localhost", "root", "", "pixelplayground");
+
+        }catch (Exception $e){
+          $error = "niet goed";
+          die($error);
+        }
+            $user = $_POST['gebruikersnaam'];
+            $pass = password_hash($_POST['wachtwoord'],  PASSWORD_DEFAULT);
+           
+            $sql = "UPDATE gebruikers (gebruikersnaam, wachtwoord) VALUES ('$user', '$pass')";
+?>
+
   <body>
   <main class="account-page">
     <h1>Mijn Account</h1>
