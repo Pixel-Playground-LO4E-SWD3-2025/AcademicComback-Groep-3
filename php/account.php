b993ee5fe5b5ebdb3256926ef883e037348b7105
t <6045566@mborijnland.nl>
2025-06-06 16:42:04 +0200

    acc veraneren
    inloggen is gefixed, naaam en ww is te veranderen en het laat nu ook je naam zien bij e gegevens pagina

--- php/account.php
+++ php/account.php
@@ -1,59 +1,48 @@
-  <?php require_once '../partials/header.php'; 
-  
+  <?php require_once '../partials/header.php';
+
   ?>
 
-  <?php if (isset($_POST['change-pass'])) {
-    // if (!empty($_POST['gebruikersnaam']) && !empty($_POST['wachtwoord'])) {
-        try {
-            $conn = new mysqli("localhost", "root", "", "pixelplayground");
+  <?php
 
-        }catch (Exception $e){
-          $error = "niet goed";
-          die($error);
-        }
-            // $user = $_POST['gebruikersnaam'];
-            $pass = password_hash($_POST['wachtwoord'],  PASSWORD_DEFAULT);
-           $username = $_SESSION ['username'];
-            $sql = "UPDATE gebruikers SET wachtwoord = '$pass' WHERE gebruikersnaam = '$username'";
-            $sql = "UPDATE gebruikers SET gebruikersnaam = '$username' WHERE gebruikersnaam = '$username'";
-            $conn->query($sql);
-      }
-    // }
-   
-?>
+
+  $username = $_SESSION['username'];
+  // }
+
+  ?>
 
   <body>
-  <main class="account-page">
-    <h1>Mijn Account</h1>
+    <main class="account-page">
+      <h1>Mijn Account</h1>
 
-    <section class="account-section">
-      <h2>Gebruikersgegevens</h2>
-      <article>
-        <p><strong>Naam:</strong></p>
-        <p><strong>Email:</strong></p>
-        <p><strong>Gebruikersnaam:</strong> </p>
-      </article>
-    </section>
+      <section class="account-section">
+        <h2>Gebruikersgegevens</h2>
+        <article>
+          <p><strong>Naam:</strong></p>
+          <?php echo $username ?>
+          <p><strong>Gebruikersnaam:</strong> </p>
+        </article>
+      </section>
 
-  <form action="" id="wachtwoord" method="post">
-    wachtwoord
-  <input type="text" name="wachtwoord">
-  <input type="submit" name='change-pass'>
-</form>
-  <form action="" id="uname" method="post">
-    Username
-    <input type="text" name="gebruikersnaam">
-    <input type="submit" name='submit'>
-  </form>
+      <form action="" id="wachtwoord" method="post">
+        wachtwoord
+        <input type="text" name="wachtwoord">
+        <input type="submit" name='change-pass'>
+      </form>
+      <form action="" id="uname" method="post">
+        Username
+        <input type="text" name="gebruikersnaam">
+        <input type="submit" name='submit'>
+      </form>
 
-    <section class="account-section">
-      <h2>Acties</h2>
-      <article>
-        <button id="wachtbutton">Wachtwoord wijzigen</button>
-        <button id="unamebutton">Naam wijzigen</button>
-      </article>
-    </section>
-  </main>
-</body>
-</html>
-<?php require_once '../partials/footer.php'; ?>
+      <section class="account-section">
+        <h2>Acties</h2>
+        <article>
+          <button id="wachtbutton">Wachtwoord wijzigen</button>
+          <button id="unamebutton">Naam wijzigen</button>
+        </article>
+      </section>
+    </main>
+  </body>
+
+  </html>
+  <?php require_once '../partials/footer.php'; ?>
--- php/login.php
+++ php/login.php
@@ -10,10 +10,11 @@ if (isset($_POST['submit']))
     $sql = "SELECT * FROM gebruikers WHERE gebruikersnaam = '$user'";
     $result = $conn->query($sql);
 
+
     if ($result->num_rows == 1)
     {
-      echo "Login gegevens juist";
-      $row = $result->fetch_assoc(); // $row = [17, 'tabesh', 'wachtwoord']
+      $row = $result->fetch_object();
+      $hashUitDatabase = $row->wachtwoord;
       if (password_verify($pass, $hashUitDatabase))
       {
         // hier kun je eventueel een sessie starten
@@ -43,43 +44,43 @@ if (isset($_POST['submit']))
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
 
 <body>
 
-    <main>
-        <section class="login-page">
-            <article class="login-box">
-                <form method="post">
-                    <h2>Inloggen</h2>
+  <main>
+    <section class="login-page">
+      <article class="login-box">
+        <form method="post">
+          <h2>Inloggen</h2>
 
-                    <fieldset class="input-box">
-                        <input type="text" name="gebruikersnaam" required placeholder="gebruikersnaam" />
-                    </fieldset>
+          <fieldset class="input-box">
+            <input type="text" name="gebruikersnaam" required placeholder="gebruikersnaam" />
+          </fieldset>
 
-                    <fieldset class="input-box">
-                        <input type="password" name="wachtwoord" required placeholder="Wachtwoord" />
-                    </fieldset>
+          <fieldset class="input-box">
+            <input type="password" name="wachtwoord" required placeholder="Wachtwoord" />
+          </fieldset>
 
-                    <section class="remember-forgot">
-                        <label><input type="checkbox" name="remember" /> Herinner mij</label>
-                        <a href="#">Wachtwoord vergeten?</a>
-                    </section>
+          <section class="remember-forgot">
+            <label><input type="checkbox" name="remember" /> Herinner mij</label>
+            <a href="#">Wachtwoord vergeten?</a>
+          </section>
 
-                    <input type="submit" name="submit">Login</input>
+          <input type="submit" name="submit">Login</input>
 
-                    <section class="register-link">
-                        <p>Geen account? <a href="sign.php">Account aanmaken</a></p>
-                    </section>
-                </form>
-            </article>
-        </section>
-    </main>
+          <section class="register-link">
+            <p>Geen account? <a href="sign.php">Account aanmaken</a></p>
+          </section>
+        </form>
+      </article>
+    </section>
+  </main>
 
-    <?php require_once '../partials/footer.php'; ?>
+  <?php require_once '../partials/footer.php'; ?>
 
 </body>
 
