7989f7e89ff562e6e0d4f28efbe139d61dcb41f1
t <6045566@mborijnland.nl>
2025-06-10 14:31:35 +0200

    begin game

---
+++ php/flappy.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flappy Bird</title>
+    <script src="../js/flappy.js" defer></script>
+
+    <link rel="stylesheet" href="../CSS/style2.css">
+</head>
+
+<body>
+    <canvas id="board"></canvas>
+</body>
+
+</html>
--- php/games.php
+++ php/games.php
@@ -2,7 +2,7 @@
   <main>
     <section class="intro-text">
       <p>
-       Speel een van onze geweldige spelen!
+        Speel een van onze geweldige spelen!
       </p>
     </section>
 
@@ -15,6 +15,11 @@
         <img src="../img/Snake.png" alt="Game afbeelding 2">
         <p>Speel snake hier!</p>
       </div>
+      <div class="image-box">
+        <li><a href="flappy.php">Flappy</a></li>
+        <img src="../img/flappy.jpg" alt="Game afbeelding 3">
+        <p>flappybird hier!</p>
+      </div>
     </section>
   </main>
-<?php require_once '../partials/footer.php'; ?>
+  <?php require_once '../partials/footer.php'; ?>
